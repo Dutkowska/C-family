0868339936096d4719d949b93f8b180f18085817
Dutkowska <dutkowska@yahoo.com>
2019-01-04 12:41:10 +0100

    Editing eucgame

--- Spoj/eucgame-gra_euklidesa.cpp
+++ Spoj/eucgame-gra_euklidesa.cpp
@@ -1,3 +1,4 @@
+//greatest common divisor - nwd: just cout a;
 #include <iostream>
 using namespace std;
 int main()
@@ -8,12 +9,10 @@ int main()
     {
         int a, b;
         cin >> a >> b;
-        while(a<b || b<a)
+        while(a!=b)
         {
-            if(a<b)
-                b-=a;
-            else
-                a-=b;
+            if(a<b) b-=a;
+            else a-=b;
         }
         cout << a+b << endl;
     }
