8a4c08219c9cd3947ff69e3a6cb28909db75e753
Dutkowska <dutkowska@yahoo.com>
2019-01-15 08:31:20 +0100

    Improvement: matrix-inverse
    correction of adjugate function, make better (recursive) function for determinant

--- algorithms_cpp/matrix-inverse.cpp
+++ algorithms_cpp/matrix-inverse.cpp
@@ -1,6 +1,5 @@
 //A−1 = adj(A) / det(A) for matrix 3x3
 #include <iostream>
-#include <math.h>
 using namespace std;
 void add_mat(int A[][20], int n)
 {
@@ -8,64 +7,76 @@ void add_mat(int A[][20], int n)
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++) cin >> A[i][j];
 }
-int det(int A[][20], int n)
+
+int adjugate(int A[][20], int temp[20][20], int n, int p, int q)
 {
-    if(n==2)
-        return A[0][0]*A[1][1]-A[1][0]*A[0][1];
-    else if(n==1)
-        return A[0][0];
-    else if (n==3)
-        return A[0][0]*A[1][1]*A[2][2]+A[1][0]*A[2][1]*A[0][2]+A[2][0]*A[0][1]*A[1][2]-A[1][0]*A[0][1]*A[2][2]-A[0][0]*A[2][1]*A[1][2]-A[2][0]*A[1][1]*A[0][2];
-    else
-        return 0; // not yet
-}
-int cofactor2(int A[][20], int temp[20][20], int n, int p, int q)
-{
-    //delete 0 from the start;
     int ii=0, jj=0;
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
             if(i!=p && j!=q)
-                temp[ii][jj++]=A[i][j];
-            if(jj==n-1)//new col
             {
-                jj=0;
-                ii++;
+                temp[ii][jj++]=A[i][j];
+                if(jj==n-1)//new col
+                {
+                    jj=0;
+                    ii++;
+                }
             }
         }
     }
 }
+
+int det(int A[][20], int n)
+{
+    if(n==1)
+        return A[0][0];
+    else
+    {
+        int de=0;
+        int B[20][20], sign=1;
+        for(int i=0; i<n; i++)
+        {
+            adjugate(A, B, n, 0, i);
+            de+=A[0][i]*sign*det(B, n-1);
+            sign=-sign;
+        }
+        return de;
+    }
+    /* or not with recursive:
+    else if(n==2)
+        return A[0][0]*A[1][1]-A[1][0]*A[0][1];
+    else if (n==3)
+        return A[0][0]*A[1][1]*A[2][2]+A[1][0]*A[2][1]*A[0][2]+A[2][0]*A[0][1]*A[1][2]-A[1][0]*A[0][1]*A[2][2]-A[0][0]*A[2][1]*A[1][2]-A[2][0]*A[1][1]*A[0][2];*/
+}
+
 void cofactor(int A[][20], int n)
 {
     int B[20][20];
-    for(int i=0; i<n; i++)
+    if(det(A, n)!=0)
     {
-        for(int j=0; j<n; j++)
+        for(int i=0; i<n; i++)
         {
-            //int temp[20][20];
-            //B[i][j]=pow(-1, i+j)*det(cofactor2(A, temp, 3, i, j), 2); //*A[without ij[]
-            //delete 0 from cof2;
-            //delete temp;
+            for(int j=0; j<n; j++)
+            {
+                int temp[20][20];
+                int sign;
+                //B[i][j]=sign*|adjA|/|A|;
+            }
         }
     }
+    else cout << "Wrong determinant for inverse matrix (det(A)=0)!";
 }
-// if det == 0 return false
 int main()
 {
-    int A[20][20], n=3;
+    int A[20][20], B[20][20], n, de;
+    cout << "Enter the number of columns of a matrix n x n: " << endl;
+    cin >> n; // for that moment: try 3;
     add_mat(A,n);
-    int d;
-    d=det(A, n);
-    cout << "Det(A) = " << d << endl;
-    /*int temp[20][20];
-    cofactor2(A, temp, n, 0, 0);
-    for(int i=0; i<n-1; i++)
-    {
-        for(int j=0; j<n-1; j++) cout << temp[i][j] << " ";
-    }
-    */
+    de=det(A, n);
+    cout << "Det(A) = " << de << endl;
+    cofactor(A, n);
     return 0;
 }
 
