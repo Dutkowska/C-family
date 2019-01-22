fb4e71cc11e02ab03189a06de7a1fbba4eaf62f2
Dutkowska <dutkowska@yahoo.com>
2019-01-22 11:36:28 +0100

    function improvement

--- algorithms_cpp/permutation_letters-with_and_without_repetitions.cpp
+++ algorithms_cpp/permutation_letters-with_and_without_repetitions.cpp
@@ -1,10 +1,9 @@
-// task have to be in C - but I made this in C++ for my own uses.
 #include <iostream>
 using namespace std;
 int k, num[10];
 char chars[10];
 
-void perm(int k, int pos)
+void perm(int k, int pos, string repet)
 {
     if(k!=pos)
     {
@@ -14,9 +13,14 @@ void perm(int k, int pos)
             {
                 // num[i] = 1,0 in comments = > permutations with repetitions
                 chars[pos]=(char)(i+'a');
-                num[i]=1;
-                perm(k, pos+1);
-                num[i]=0;
+                if(repet=="with")
+                    perm(k, pos+1, repet);
+                else if(repet=="without")
+                {
+                    num[i]=1;
+                    perm(k, pos+1, repet);
+                    num[i]=0;
+                }
             }
         }
     }
@@ -31,6 +35,9 @@ void perm(int k, int pos)
 int main()
 {
     cin >> k;
-    perm(k, 0);
+    cout << "With repetitions: " << endl;
+    perm(k, 0, "with");
+    cout << endl << "Without repetitions: " << endl;
+    perm(k, 0, "without");
     return 0;
 }
